afb224467cbba7c54a86d586f09c7361d06e17ac
Karris  Black <karris.b@gmail.com>
2020-12-30 17:11:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    count_down = ["left", "right", "round"]  
+    count_down = ["wow", "right", "round"]  
     count_down[0]
 
 end
