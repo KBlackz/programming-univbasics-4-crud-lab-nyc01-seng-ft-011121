d67ba2ae73927584aa5028ed167c4bfd7352440b
Karris  Black <karris.b@gmail.com>
2020-12-30 17:03:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def remove_element_from_start_of_array(array)
     time_keep = ["earth", "world", "wow"]
-    time_keep.unshift
+    time_keep.unshift()
 end
 
 def retrieve_element_from_index(array, index_number)
