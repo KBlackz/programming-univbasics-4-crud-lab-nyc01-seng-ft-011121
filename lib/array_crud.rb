ae81b62d685781f681bd021d26f727931e82625d
Karris  Black <karris.b@gmail.com>
2020-12-30 17:10:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+    count_down = ["left", "right", "round"]  
+    count_down[0]
+
 end
 
 def retrieve_last_element_from_array(array)
