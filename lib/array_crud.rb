6f1a2135936316775f827eb039a63f10e3839c9d
Karris  Black <karris.b@gmail.com>
2020-12-30 15:21:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,8 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  cars = ["Gle", "M5", "E63", "RS5"]
-  cars.push("array", "element")
+  cars.push("C63")
 
 def add_element_to_start_of_array(array, element)
   
