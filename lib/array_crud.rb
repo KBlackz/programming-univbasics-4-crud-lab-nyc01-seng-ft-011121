57a4ec3752eff5356c0f7085ff920bf4e298cdf6
Karris  Black <karris.b@gmail.com>
2020-12-30 14:53:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,8 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- 
-  
+  cars = ["Gle", "M5", "E63", "RS5"]
+  cars<< "Benz"
 end
 
 def add_element_to_start_of_array(array, element)
